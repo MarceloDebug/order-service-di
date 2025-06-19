41cf7a8348220f04a20c6f08ea4e1341c86b1e2b
MarceloDebug <marcelinho.soares300@gmail.com>
2025-06-19 11:27:02 -0300

    Initial commit

---
+++ src/main/java/com/marcelodebug/order_service_di/OrderServiceDiApplication.java
@@ -0,0 +1,13 @@
+package com.marcelodebug.order_service_di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcelodebug/order_service_di/OrderServiceDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcelodebug.order_service_di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
